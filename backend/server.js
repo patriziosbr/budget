90849f888ca17cbdc33b209308745a7eb408d95c
patriziosbr <sbrozzi.patrizio@gmail.com>
2025-05-07 00:13:43 +0200

    cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors(
     {
-    origin: 'http://localhost:3000/', // Replace with your frontend URL
+    origin: 'https://budget.onrender.com/', // Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     }
